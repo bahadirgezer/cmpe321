5e145fbcd6b7c05f4958b1b15d206ff2a38d0769
bahadirgezer <bahadirgezerbg@gmail.com>
2023-05-31 19:06:27 +0300

    fix main.py

--- project3/src/main.py
+++ project3/src/main.py
@@ -1,32 +1,14 @@
-import database.query as q
-from database.database import Database, Column
-import tkinter as tk
+from database.database import Database
 from gui.app import App
-
-
-def loop(db: Database):
-    keyboard_input: str = ""
-    while keyboard_input != "exit":
-        print(db.execute_query(keyboard_input))
-        keyboard_input = input("> ")
-    db.disconnect()
 
 
 if __name__ == '__main__':
     db = Database()
     db.connect()
-    db.use_schema("movie")
-    db.create_table("audience", columns=[
-        Column(name="id", data_type="int", primary_key=True, auto_increment=True),
-        Column(name="name", data_type="varchar", length=50),
-        Column(name="age", data_type="int"),
-        Column(name="username", data_type="varchar", length=50, unique=True),
-    ])
+    db.use_schema("test")
 
-    app = App()
+    app = App(db)
     app.mainloop()
-    # loop(db)
-
 
     input()
     db.disconnect()
